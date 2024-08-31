b7e48b2caf52c6f78bbd62390faa332c898aa806
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-08-31 22:06:05 +0600

    Start work on hero section

---
+++ src/Components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="h-auto md:h-[40rem] w-full flex justify-center items-center relative rounded-md overflow-hidden mx-auto py-10 md:py-10">
+      Hero Section
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import HeroSection from "@/Components/HeroSection";
+
 export default function Home() {
   return (
     <>
-      <main className="bg-slate-900 h-screen">
+      <main className="bg-slate-900 min-h-screen bg-grid-white/[0.02] antialiased text-white">
         <h1 className="text-3xl font-semibold text-center">Muhammad Sefat</h1>
+        <HeroSection />
       </main>
     </>
   );
