d495ec05a63fe431650fe4b6a577ff4ee11c1c28
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-08-31 22:49:36 +0600

    Complete Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <>
       <main className="bg-slate-900 min-h-screen bg-grid-white/[0.02] antialiased text-white">
-        <h1 className="text-3xl font-semibold text-center">Muhammad Sefat</h1>
         <HeroSection />
       </main>
     </>
